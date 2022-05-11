5f53139aee0ebe6fd4ad0bdd46128b2372d54bf9
fyirass <firas.mansour@esprit.tn>
2022-05-11 17:05:03 +0100

    FaceAnalyticsApp commit

--- deepfacesuite/settings.py
+++ deepfacesuite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'y2!nc-hr+26tl7rx#p-_oc6yednw8&hv^!-cfg%o*=_@kg_efa'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','deepfacesuite.com']
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
